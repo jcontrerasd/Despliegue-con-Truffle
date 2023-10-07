09d62d2a92d4f6474b1aeb7fdd9a1710cd70c5fe
jcontrerasd <jaime.contreras.d@gmail.com>
2023-10-07 19:04:10 -0300

    primera carga de archivis

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const billetera2billetera = artifacts.require("billetera2billetera");
+
+module.exports = function(deployer) {
+  deployer.deploy(billetera2billetera);
+};
---
+++ truffle-config.js
@@ -0,0 +1,53 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+
+
+require("dotenv").config();
+
+const private_key = process.env.SIGNER_PRIVATE_KEY;
+
+module.exports = {
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: process.env.ETH_SCAN_API_KEY,
+    polygonscan: process.env.POLYGON_SCAN_API_KEY,
+    bscscan: process.env.BSC_SCAN_API_KEY
+  },
+  networks: {
+    ethereum_goerli_testnet: {
+      provider: () => new HDWalletProvider(private_key, process.env.ETH_GOERLI_TESTNET_RPC),
+      network_id: 5,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    polygon_mumbai_testnet: {
+      provider: () => new HDWalletProvider(private_key, process.env.POLYGON_MUMBAI_TESTNET_RPC),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    binance_bsc_testnet: {
+      provider: () => new HDWalletProvider(private_key, process.env.BSC_TESTNET_RPC),
+      network_id: 97,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.1",
+      parser: "solcjs",
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        }
+      }
+    }
+  }
+}
